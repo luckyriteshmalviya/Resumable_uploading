c1a5a04d98245d7420aee8a0a9850690efc1db8f
Ritesh Malviya <99033374+luckyriteshmalviya@users.noreply.github.com>
2023-07-03 15:24:42 +0530

    Update App.jsx

--- file_upload/src/App.jsx
+++ file_upload/src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import FileUpload from './upload'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
-  // function handleUpload(e){
-  //   console.log(e.target.files[0])
-
-//  }
   return (
     <>
       <div>
-      {/* <input type='file' onChange={handleUpload} /> */}
       <FileUpload />
       </div>
     </>
